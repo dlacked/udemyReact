80df586aac833d0d4ae4d4c095667b5265eb602b
임창용 <imcy0104@gmail.com>
2025-06-01 11:30:39 +0900

    Components에 동적 내용 추가

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,16 @@
+const reactDescription = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {reactDescription[genRandomInt(reactDescription.length)]} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
