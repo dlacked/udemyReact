15127df98944183b7dcd195fc807d18ea232f495
임창용 <imcy0104@gmail.com>
2025-06-01 16:12:06 +0900

    이벤트 처리

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,5 +1,9 @@
 function TabButton({children}) {
-    return <li><button>{children}</button></li>
+    function handleClick() {
+        console.log('Hello World!');
+    }
+
+    return <li><button onClick={handleClick}>{children}</button></li>
 }
 
 export default TabButton;
