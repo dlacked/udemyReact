77dd6cde4f95b957f935ed93d548da392e9018dc
임창용 <imcy0104@gmail.com>
2025-06-05 00:30:59 +0900

    UI가 업데이트되지 않는 이유

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -4,9 +4,12 @@ import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+  let tabContent = 'Please click a button';
+
   function handleSelect(selectedButton) {
     //selectedButton => 'components', 'jsx', 'props', 'state'
-    console.log(selectedButton);
+    tabContent = selectedButton;
+    console.log(tabContent)
   }
 
   return (
@@ -29,7 +32,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          Dynamic Content
+          {tabContent}
         </section>
       </main>
     </div>
