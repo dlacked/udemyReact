7dd79d917c138078f022fab4c28efaaa7c81361b
임창용 <imcy0104@gmail.com>
2025-06-05 00:14:51 +0900

    이벤트 함수에 커스텀 인자 전달하기

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,9 +1,5 @@
-function TabButton({children}) {
-    function handleClick() {
-        console.log('Hello World!');
-    }
-
-    return <li><button onClick={handleClick}>{children}</button></li>
+function TabButton({children, onSelect }) {
+    return <li><button onClick={onSelect}>{children}</button></li>
 }
 
 export default TabButton;
